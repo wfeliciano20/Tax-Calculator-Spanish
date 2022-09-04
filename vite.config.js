0930928ef24069623ff9ae5652fa3dd5a181bc2f
wfeliciano20 <william.feliciano2@gmail.com>
2022-09-03 20:46:34 -0400

    deploytest4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-base = /Tax-Calculator-Spanish/;
+base: /Tax-Calculator-Spanish/;
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
